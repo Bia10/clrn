c3bb259f48b68ba3f784e7177723c5d22a61d917
clrnmail <clrnmail@gmail.com>
2012-07-22 20:00:01 +0000

    refactoring

--- projects/sync/common/proto/src/StreamHelpers.cpp
+++ projects/sync/common/proto/src/StreamHelpers.cpp
@@ -2,18 +2,18 @@
 
 std::ostream& operator << (std::ostream& stream, const data::Table& object)
 {
-	stream << object.ShortDebugString();
+	stream << object.DebugString();
 	return stream;
 }
 
 std::ostream& operator<<(std::ostream& stream, const jobs::Job& object)
 {
-	stream << object.ShortDebugString();
+	stream << object.DebugString();
 	return stream;
 }
 
 std::ostream& operator<<(std::ostream& stream, const packets::Packet& object)
 {
-	stream << object.ShortDebugString();
+	stream << object.DebugString();
 	return stream;
 }
