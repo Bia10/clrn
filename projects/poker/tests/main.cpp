2d04b0806a9db2e8c34e851c87fd9f8fa2ab4d01
clrnmail <clrnmail@gmail.com>
2013-01-21 06:31:12 +0000

    tests and protocols added

--- projects/poker/tests/main.cpp
+++ projects/poker/tests/main.cpp
@@ -1,5 +1,9 @@
 #include "CombinationsCalculator.h"
 #include "../../evaluator/HandEval.h"
+#include "../../evaluator/SevenEval.h"
+#include "../protocols/inc/packet.pb.h"
+
+#include "gtest/gtest.h"
 
 #include <iostream>
 
@@ -7,8 +11,11 @@
 
 using namespace clnt;
 
-int main()
+TEST(HandEval, Old)
 {
+	packets::Packet packet;
+	
+
 	HandEval *eval = new HandEval();
 	int holeCards[4] = {0, 11, 37, 38}; // Two pairs of hole cards, the first
 	// player has the Ace of Spades and the
@@ -17,7 +24,30 @@ int main()
 	// 5 of Diamonds.
 
 	// The equity should be approximately 45.33% versus 54.67%.
-	printf("\n%s\n", eval->computePreFlopEquityForSpecificHoleCards(holeCards, 1));
+	printf("\n%s\n", eval->computePreFlopEquityForSpecificHoleCards(holeCards, 2));
+	eval->timeRankMethod();
+	delete eval;
+
+	SevenEval *sEval = new SevenEval();
+	printf("%i\n", sEval->getRankOfSeven(0, 1, 2, 3, 4, 5, 6)); // Ace quad with kings, 7452
+	printf("%i\n", sEval->getRankOfSeven(5, 6, 2, 4, 3, 0, 1)); // Shuffled ace quad with kings, again 7452
+	printf("%i\n", sEval->getRankOfSeven(0, 4, 8, 12, 16, 20, 24)); // Spade royal flush, 7462
+	printf("%i\n", sEval->getRankOfSeven(51, 47, 43, 39, 30, 26, 22)); // 49, the worst hand.
+	delete sEval;
+}
+
+TEST(Test, Sample)
+{
+	EXPECT_EQ(1, 1);
+	HandEval *eval = new HandEval();
+	int holeCards[4] = {0, 11, 37, 38}; // Two pairs of hole cards, the first
+	// player has the Ace of Spades and the
+	// Queen of Clubs, while the second
+	// player has the 5 of Hearts and the
+	// 5 of Diamonds.
+
+	// The equity should be approximately 45.33% versus 54.67%.
+	printf("\n%s\n", eval->computePreFlopEquityForSpecificHoleCards(holeCards, 2));
 
 	Calculator calc;
 
@@ -94,5 +124,4 @@ int main()
 
 		const Combination::Value rank = Combination::FromEval(calc.GetRank(cards));
 	}
-	return 0;
 }
