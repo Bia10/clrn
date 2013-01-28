34600e1386d961b836e86c7eb9ac9fc7753bc83f
clrnmail@gmail.com <clrnmail@gmail.com@9c4a8978-c16f-196d-58d4-48b9da28680c>
2013-01-28 15:52:46 +0000

    one more test

--- trunk/projects/common/db/tests/DBTests.cpp
+++ trunk/projects/common/db/tests/DBTests.cpp
@@ -45,3 +45,82 @@ TEST(SQLite, InsertFetch)
 		++row;
 	}
 }
+
+
+void ReadThread()
+{
+	sql::SQLiteDataBase db(g_Logger);
+	db.Open("test.db");
+
+	for (;;)
+	{
+		try
+		{
+			const sql::Recordset::Ptr recordset = db.Fetch("select * from emp");
+			int row = 0;
+			while (!recordset->Eof())
+			{
+				const int value = recordset->Get<int>(0);
+				//ASSERT_EQ(value, row);
+				++(*recordset);
+				++row;
+			}
+
+			std::cout << "Read : " << row << std::endl;
+
+			boost::this_thread::interruption_point();
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+}
+
+void WriteThread()
+{
+	sql::SQLiteDataBase db(g_Logger);
+	db.Open("test.db");
+
+	db.Execute("create table emp(empno int, empname char(20));");
+	std::size_t i = 0;
+	std::size_t counter = 0;
+
+	for (;;)
+	{
+		try
+		{
+			db.BeginTransaction();
+
+			const sql::IStatement::Ptr stmnt = db.CreateStatement("insert into emp values(?, ?)");
+
+			const std::size_t rows = 1000;
+			for (std::size_t i = 0 ; i < rows; ++i)
+			{
+				const std::string name = std::string("employee_") + boost::lexical_cast<std::string>(counter);
+				*stmnt << int(counter++) << name;
+				stmnt->Execute();
+			}
+
+			db.Commit();
+			boost::this_thread::interruption_point();
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+}
+
+TEST(SQLite, Mulithread)
+{
+	fs::Remove("test.db");
+
+	boost::thread read(&ReadThread);
+	boost::thread write(&WriteThread);
+
+	boost::this_thread::interruptible_wait(10000);
+
+	read.interrupt();
+	write.interrupt();
+}
