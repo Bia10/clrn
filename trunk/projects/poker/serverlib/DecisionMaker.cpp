2b6bc59947483b21d772bce2a55b47e8b9d18cf3
clrnmail@gmail.com <clrnmail@gmail.com@9c4a8978-c16f-196d-58d4-48b9da28680c>
2013-05-16 14:47:11 +0000

    make continuation bet only with big stack

--- trunk/projects/poker/serverlib/DecisionMaker.cpp
+++ trunk/projects/poker/serverlib/DecisionMaker.cpp
@@ -93,7 +93,7 @@ void DecisionMaker::MakeDecision(const pcmn::Player& player, const pcmn::Player:
 
         // continuation bet
         std::vector<float> out;
-        if (context.m_Street == 1 && params.m_BotStyle == pcmn::Player::Style::Aggressive && params.m_BotAverageStyle != pcmn::Player::Style::Aggressive && params.m_Danger == pcmn::Danger::Low && params.m_BetSize < pcmn::BetSize::Normal)
+        if (context.m_Street == 1 && params.m_BotStyle == pcmn::Player::Style::Aggressive && params.m_BotAverageStyle != pcmn::Player::Style::Aggressive && params.m_Danger == pcmn::Danger::Low && params.m_BetSize < pcmn::BetSize::Normal && context.m_Pot * 2 < player.Stack())
         {
             out = boost::assign::list_of(0.0f)(0.0f)(1.0f);
             LOG_TRACE("Making continuation bet: [%s]") % out;
