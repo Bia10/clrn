f468fc8b427b1bd85fdd377420bc4c52cd6eff6e
clrnmail <clrnmail@gmail.com>
2013-05-17 11:38:22 +0000

    short stack strategy decisions

--- projects/poker/data/neuro_data.sql
+++ projects/poker/data/neuro_data.sql
@@ -217,4 +217,31 @@ WHERE  win IN ( 3 )
        AND bot_avg_style IN ( 0, 1, 2 ) 
        AND bot_style IN ( 0, 1, 2 ) 
        AND bot_stack IN ( 1, 2 ) 
-       AND decision IN ( 0 ) 	  	   
+       AND decision IN ( 0 ) 	 
+
+-- raise hude bets with small stack from later position and normal rate
+UPDATE decisions 
+SET    decision = 2 
+WHERE  win IN ( 2 ) 
+       AND position IN ( 2 ) 
+       AND bet IN ( 4 ) 
+       AND players IN ( 2 ) 
+       AND danger IN ( 0, 1 ) 
+       AND bot_avg_style IN ( 0, 1 ) 
+       AND bot_style IN ( 0 ) 
+       AND bot_stack IN ( 0 ) 
+       AND decision IN ( 0 ) 	   
+	   
+-- all in on preflop with small stack and >=normal win	   
+-- raise hude bets with small stack from later position and normal rate
+UPDATE decisions 
+SET    decision = 2 
+WHERE  win IN ( 2, 3, 4, 5 ) 
+       AND position IN ( 2 ) 
+       AND bet IN ( 0, 1, 2, 3, 4 ) 
+       AND players IN ( 0, 1, 2 ) 
+       AND danger IN ( 0, 1 ) 
+       AND bot_avg_style IN ( 0, 1 ) 
+       AND bot_style IN ( 0 ) 
+       AND bot_stack IN ( 0 ) 
+       AND decision IN ( 0 ) 	
