6b37334580247c9c17514c3f65440f48a27a8ef2
clrnmail@gmail.com <clrnmail@gmail.com@9c4a8978-c16f-196d-58d4-48b9da28680c>
2013-05-21 15:01:29 +0000

    last commit reverted call high bets with normal win and three peoples on preflop

--- trunk/projects/poker/common/inc/TableLogic.h
+++ trunk/projects/poker/common/inc/TableLogic.h
@@ -61,11 +61,6 @@ public:
         //! Actions list
         typedef std::vector<ActionDesc> List;
 
-        ActionDesc()
-            : m_Name()
-            , m_Value()
-            , m_Amount()
-        {}
         ActionDesc(const std::string& name, pcmn::Action::Value value, unsigned amount)
             : m_Name(name)
             , m_Value(value)
@@ -109,7 +104,7 @@ public:
     TableLogic(ILog& logger, ITableLogicCallback& callback, const Evaluator& evaluator);
 
     //! Push action
-    void PushAction(const std::string name, Action::Value action, unsigned amount);
+    void PushAction(const std::string& name, Action::Value action, unsigned amount);
 
     //! Set player info
     void SetPlayerStack(const std::string& name, unsigned stack);
@@ -137,9 +132,6 @@ public:
 
     //! Is round finished
     bool IsRoundCompleted() const { return m_IsRoundFinished; }
-
-    //! Is table logic state valid
-    bool IsValid() const { return m_State != State::Uninited; }
 
 private: 
 
@@ -191,21 +183,6 @@ private:
     //! Parse player loose
     void ParsePlayerLoose(Player& current, BetSize::Value lastBigBet, Action::Value action);
 
-    //! Try to resolve player actions
-    void ValidateSequenceAndTryToResolve(const std::string& name);
-
-    //! Try to resolve unexpected small blind
-    void TryToResolveUnexpectedSmallBlind(const std::string& name);
-
-    //! Handle round finish
-    void HandleRoundFinish();
-
-    //! Pop action 
-    ActionDesc PopAction();
-
-    //! Check for new round
-    void CheckForNewRound(Action::Value action);
-
 private:
 
     //! Logger reference
@@ -252,9 +229,6 @@ private:
 
     //! Is round finished
     bool m_IsRoundFinished;
-
-    //! Last player in queue before raise
-    std::string m_LastBeforeRaise;
 };
 
 }
--- trunk/projects/poker/data/neuro_data.sql
+++ trunk/projects/poker/data/neuro_data.sql
@@ -335,3 +335,16 @@ WHERE  win IN ( 4 )
        AND bot_style IN ( 1, 2 ) 
        AND bot_stack IN ( 1, 2 ) 
        AND decision IN ( 0, 2 ) 		   
+	   
+-- call high bets with normal win and three peoples on preflop
+UPDATE decisions 
+SET    decision = 1
+WHERE  win IN ( 2 ) 
+       AND position IN ( 0, 1, 2 ) 
+       AND bet IN ( 2, 3 ) 
+       AND players IN ( 1, 2, 3 ) 
+       AND danger IN ( 0, 1 ) 
+       AND bot_avg_style IN ( 0, 1, 2 ) 
+       AND bot_style IN ( 0 ) 
+       AND bot_stack IN ( 1, 2 ) 
+       AND decision IN ( 0 ) 		   
