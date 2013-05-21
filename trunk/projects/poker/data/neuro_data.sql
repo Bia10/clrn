fd8d2e28e86583095371454bd8b1d281f8af739d
clrnmail@gmail.com <clrnmail@gmail.com@9c4a8978-c16f-196d-58d4-48b9da28680c>
2013-05-21 17:38:26 +0000

    all in on preflop with small stack and >=normal win

--- trunk/projects/poker/data/neuro_data.sql
+++ trunk/projects/poker/data/neuro_data.sql
@@ -219,7 +219,7 @@ WHERE  win IN ( 3 )
        AND bot_stack IN ( 1, 2 ) 
        AND decision IN ( 0 ) 	 
 
--- raise hude bets with small stack from later position and normal rate
+-- raise huge bets with small stack from later position and normal rate
 UPDATE decisions 
 SET    decision = 2 
 WHERE  win IN ( 2 ) 
@@ -240,7 +240,7 @@ WHERE  win IN ( 2, 3, 4, 5 )
        AND bet IN ( 0, 1, 2, 3, 4 ) 
        AND players IN ( 0, 1, 2 ) 
        AND danger IN ( 0, 1 ) 
-       AND bot_avg_style IN ( 0, 1 ) 
+       AND bot_avg_style IN ( 0, 1, 2 ) 
        AND bot_style IN ( 0 ) 
        AND bot_stack IN ( 0 ) 
        AND decision IN ( 0 ) 	
