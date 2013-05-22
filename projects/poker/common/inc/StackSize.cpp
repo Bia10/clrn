3c8d30c17436a2cc4f2402641a5486420dc1e07f
clrnmail <clrnmail@gmail.com>
2013-05-22 10:18:03 +0000

    small stack size detection optimization

--- projects/poker/common/inc/StackSize.cpp
+++ projects/poker/common/inc/StackSize.cpp
@@ -15,12 +15,15 @@ namespace pcmn
 #undef CASE
 	}
 
-	StackSize::Value StackSize::FromValue(unsigned value, unsigned bigBlind, unsigned maxStack, unsigned pot, unsigned maxBet)
+	StackSize::Value StackSize::FromValue(unsigned value, unsigned bigBlind, unsigned maxStack, unsigned minStack, unsigned pot, unsigned maxBet)
 	{
 		if (value < bigBlind * 10)
 			return StackSize::Small;
 
         if (value < (pot - maxBet) / 2)
+            return StackSize::Small;
+
+        if (value < minStack * 3 / 2)
             return StackSize::Small;
 
         if (value > bigBlind * 20 || value > maxStack * 2 / 3)
