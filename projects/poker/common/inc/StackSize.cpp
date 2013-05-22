14a089de067334852b2148af3c1fa33a760ec28e
clrnmail <clrnmail@gmail.com>
2013-05-22 13:31:06 +0000

    stack size calculation fix

--- projects/poker/common/inc/StackSize.cpp
+++ projects/poker/common/inc/StackSize.cpp
@@ -23,10 +23,18 @@ namespace pcmn
         if (value < (pot - maxBet) / 2)
             return StackSize::Small;
 
-        if (value > bigBlind * 20 || value > minStack + (maxStack - minStack) / 2)
+        const unsigned diff = maxStack - minStack;
+
+        if (diff < value / 2 && value > bigBlind * 20)
             return StackSize::Big;
 
-        if (value < minStack * 3 / 2)
+        const unsigned medium = minStack + diff / 2;
+        if (value > medium && value > bigBlind * 20)
+            return StackSize::Big;
+
+        const unsigned lowDiff = medium - minStack;
+        const unsigned lowLimit = minStack + lowDiff / 2;
+        if (value < lowLimit)
             return StackSize::Small;
 
         return StackSize::Normal;
